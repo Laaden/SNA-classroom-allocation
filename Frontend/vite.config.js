dd389cb81649c806c76f663f2274b68cc5bbb9ed
unknown <dantethesober@gmail.com>
2025-05-09 17:31:43 +1000

    add gh-pages deployment setup on git

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/SNA-classroom-allocation/',  // 👈 Add this line
   plugins: [react()],
   server: {
     proxy: {
