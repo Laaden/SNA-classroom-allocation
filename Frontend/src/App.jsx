710e9525d79d5cf096d751cdc4e383df299079cf
Ha Ngo <nvh1211@gmail.com>
2025-05-13 15:59:18 +1000

    Adjust App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage     from "./pages/HomePage";
 import ResultPage   from "./pages/ResultPage";
 import WeightsPage  from "./pages/WeightsPage";
@@ -6,13 +6,13 @@ import AIQueryPage from './pages/AIQueryPage';
 
 function App() {
   return (
-    <Router basename="/SNA-classroom-allocation">
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/result" element={<ResultPage />} />
         <Route path="/weights" element={<WeightsPage />} />
-        <Route path="/ai-query" element={<AIQueryPage />} /> {/* Add the new route */}
+        <Route path="/ai-query" element={<AIQueryPage />} />
       </Routes>
     </Router>
   );
